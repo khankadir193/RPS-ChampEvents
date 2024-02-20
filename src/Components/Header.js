dc02ef29a173695755ef4de8d866321e235f7e79
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-20 15:46:58 +0530

    rps champ projects

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Style/Header.css';
+import HeaderImg from '../assets/images/battle/header.png';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      
+    </div>
+  )
+}
+
+export default Header;
