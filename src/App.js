d24ca80103013ac0bb6a4536c533bc3d6e76d7d0
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-22 15:02:50 +0530

    giude and event gifting has been added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import logo from './logo.svg';
 import './App.scss';
 import Header from './components/Header.js';
+import { useState } from 'react';
+import CommonButton from './components/CommonButton.js';
 
 function App() {
+  const [eventGifting, setEventGifting] = useState(false);
+  const [guide, setGuide] = useState(false);
+
+  const toggleEventGifitng = () => {
+    setEventGifting((prevState) => !prevState);
+  };
+  const toggleGuide = () => {
+    setGuide((prevState) => !prevState);
+  };
+
   return (
     <div className="App">
       <Header />
+
+      <div className="guide-btn-wrap">
+        <CommonButton
+          btnImg={"guide"}
+          width={"21vw"}
+          handleClick={toggleGuide}
+        />
+      </div>
+
+      <div className="event-gifting-wrap">
+        <CommonButton
+          btnImg={"event-gifting-btn"}
+          width={"27vw"}
+          handleClick={toggleEventGifitng}
+        />
+      </div>
+
     </div>
   );
 }
